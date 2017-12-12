4294edc37427799d078dccea7c9a760c551dcfe1
last_one <last_onephy@gmail.com>
2017-12-12 14:36:38 +0800

    create bbox_overlaps and bbox_intersections

---
+++ lib/utils/bbox.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def bbox_overlaps(boxes, query_boxes):
+
+    box_areas = ((boxes[:, 2] - boxes[:, 0] + 1) * (boxes[:, 3] - boxes[:, 1] + 1)).reshape(-1, 1)
+    query_areas = ((query_boxes[:, 2] - query_boxes[:, 0] + 1) * (query_boxes[:, 3] - query_boxes[:, 1] + 1)).reshape(1, -1)
+
+    iw = (np.minimum(boxes[:, 2:3], query_boxes[:, 2:3].transpose()) - np.maximum(boxes[:, 0:1], query_boxes[:, 0:1].transpose())).clip(min=0)
+    ih = (np.minimum(boxes[:, 3:4], query_boxes[:, 3:4].transpose()) - np.maximum(boxes[:, 1:2], query_boxes[:, 1:2].transpose())).clip(min=0)
+
+    ua = box_areas + query_areas - iw * ih
+    iou = iw * ih / ua
+
+    return iou
+
+def bbox_intersections(boxes, query_boxes):
+
+    query_areas = ((query_boxes[:, 2] - query_boxes[:, 0] + 1) * (query_boxes[:, 3] - query_boxes[:, 1] + 1)).reshape(1, -1)
+
+    iw = (np.minimum(boxes[:, 2:3], query_boxes[:, 2:3].transpose()) - np.maximum(boxes[:, 0:1], query_boxes[:, 0:1].transpose())).clip(min=0)
+    ih = (np.minimum(boxes[:, 3:4], query_boxes[:, 3:4].transpose()) - np.maximum(boxes[:, 1:2], query_boxes[:, 1:2].transpose())).clip(min=0)
+
+    intersection = iw * ih / query_areas
+
+    return intersection
